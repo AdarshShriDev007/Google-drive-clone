9134614bcfd2cd45f06294ca6d01d041d517769a
Adarsh Kumar Shrivastava <adarshshrivastava.dev@gmail.com>
2023-09-27 22:08:54 +0530

    I completed backend stored functionality with firebase

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,83 @@
 import React, { useState } from 'react';
 import "./Sidebar.css";
-import { Add, CloudQueue, DeleteOutlineOutlined, Devices, MobileScreenShare, PeopleAltOutlined, QueryBuilderOutlined, StarBorderOutlined } from '@mui/icons-material';
+import { Add, Addchart, CloudQueue, DeleteOutlineOutlined, Devices, MobileScreenShare, PeopleAltOutlined, QueryBuilderOutlined, StarBorderOutlined } from '@mui/icons-material';
+import {
+    db,
+    collection,
+    addDoc,
+    storage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from "../../firebase";
 
 const Sidebar = () => {
 
-    const [file,setFile] = useState([]);
+    const [progressVal,setProgressVal] = useState(0);
+    const [show,setShow] = useState("none");
 
     const createFile = ()=>{
         const inputFile = document.createElement("input");
         inputFile.type = "file";
         inputFile.addEventListener('change',(event)=>{
             if(event.target.files[0])
-            {
-                setFile(event.target.files[0]);
-                handleFile();
+            {  
+                handleFile(event.target.files[0]);
             }
         })
 
         inputFile.click();
     }
 
-    console.log(file);
 
-    const handleFile = ()=>{
-        if(file)
+    const handleFile = (fileData)=>{
+        if(fileData)
         {
-            
+            const storageRef = ref(storage, `files/${fileData.name}`);
+
+            const uploadTask = uploadBytesResumable(storageRef, fileData);
+
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                    const progressData = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setShow("block");
+                    setProgressVal(Math.floor(progressData));
+                
+                }, 
+                (error) => {
+                    console.error(error);
+                }, 
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                        try{
+                            addDoc(collection(db, "MyDrive"),{
+                                timestamp: new Date(),
+                                filename : fileData.name,
+                                fileURL : url,
+                                size : fileData.size
+                            });
+                            setProgressVal(0);
+                            setShow("none");
+                            
+                        }
+                        catch(error){
+                            console.error(error);
+                        }
+                    });
+                }
+            );
+
         }
     }
 
   return (
 
     <>
+       <div className="progress" style={{display: `${show}`}}>
+            <div className='progress-bar' style={{width:`${progressVal}%`}}></div>
+            <span>{progressVal}%</span>
+       </div>
+
         <div className='sidebar'>
             <button className="sidebar_btn" onClick={createFile}>
                 <Add />
---
+++ src/firebase.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    limit,
+    onSnapshot
+} from "firebase/firestore";
+
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from "firebase/storage";
+
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCcZEMSDmOCR7tqYUhnqCNcTdHTuds8cs",
+  authDomain: "drive-clone-5bc6a.firebaseapp.com",
+  projectId: "drive-clone-5bc6a",
+  storageBucket: "drive-clone-5bc6a.appspot.com",
+  messagingSenderId: "43233546111",
+  appId: "1:43233546111:web:3f0eb57f4e49aad83b544a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {
+    db,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    limit,
+    onSnapshot,
+
+    storage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL,
+
+    auth,
+    signInWithPopup,
+    provider
+}
